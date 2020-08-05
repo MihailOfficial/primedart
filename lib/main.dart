f1a7788d2839cd95cbe8c818e5e1d6afecd8a434
Mihail C <calitoiumihail@gmail.com>
2020-08-05 09:57:45 -0400

    generates primes and composites (colour coded)

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ void main() async {
   await flameUtil.fullScreen();
   final size = await Flame.util.initialDimensions();
   game = MyGame(size);
-
+  tempWidth = size.width;
   runApp(game.widget);
   TapGestureRecognizer tapper = TapGestureRecognizer();
   tapper.onTapDown = game.onTapDown;
@@ -74,17 +74,24 @@ class Test extends TextComponent{
 
   }
 }
-TextConfig regular = TextConfig(color: BasicPalette.white.color);
+TextConfig comp = TextConfig(color: BasicPalette.white.color);
+TextConfig prime = TextConfig(color: Color(0xFFFF00FF));
 double tempWidth = 0;
+
 class MyGame extends BaseGame {
-  double timerGem = 0;
+  double timerPrime = 0;
+  double timerComp = 0;
   Test test;
+  var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149];
+  var composites = [4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30, 32, 33, 34, 35, 36, 38, 39, 40, 42, 44, 45, 46, 48, 49, 50, 51, 52, 54, 55, 56, 57, 58, 60, 62, 63, 64, 65, 66, 68, 69, 70, 72, 74, 75, 76, 77, 78, 80, 81, 82, 84, 85, 86, 87, 88, 90, 91, 92, 93, 94, 95, 96, 98, 99, 100, 102, 104, 105, 106, 108, 110, 111, 112, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 128, 129, 130, 132, 133, 134, 135, 136, 138, 140, 141, 142, 143, 144, 145, 146, 147, 148, 150];
   var rng;
   MyGame(Size size) {
     this.rng = new Random();
     heightPos = size.height;
-    this.timerGem = Normal.quantile(rng.nextDouble(), mean: 3, variance: 0.7);
-    tempWidth = size.width;
+    this.timerPrime = Normal.quantile(rng.nextDouble(), mean: 3, variance: 0.7);
+    this.timerComp = Normal.quantile(rng.nextDouble(), mean: 3, variance: 0.7);
+
+    print(composites.length);
   }
     @override
     void render(Canvas c) {
@@ -93,13 +100,21 @@ class MyGame extends BaseGame {
     @override
     void update(double t) {
 
-      timerGem -= t;
-      if (timerGem < 0) {
+      timerPrime -= t;
+      timerComp -= t;
+      if (timerPrime < 0) {
         double posGem = rng.nextDouble() * heightPos;
-        int genInt = rng.nextInt(11);
-        add(test = Test(genInt.toString(), regular, tempWidth, posGem));
+        int genInt = rng.nextInt(36);
+        add(test = Test(primes[genInt].toString(), comp, tempWidth, posGem));
 
-        timerGem = Normal.quantile(rng.nextDouble(), mean: 0, variance: 0.7) + 2;
+        timerPrime = Normal.quantile(rng.nextDouble(), mean: 0, variance: 0.7) + 2;
+      }
+      if (timerComp < 0) {
+        double posComp = rng.nextDouble() * heightPos;
+        int genInt = rng.nextInt(115);
+        add(test = Test(composites[genInt].toString(), prime, tempWidth, posComp));
+
+        timerComp = Normal.quantile(rng.nextDouble(), mean: 0, variance: 0.7) + 2;
       }
       super.update(t);
   }
