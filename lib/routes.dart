405544afc0e752061a4aa563ad7f8d3e745c5ae9
Mihail C <calitoiumihail@gmail.com>
2020-08-26 00:14:50 -0400

    added left menu and login screen

---
+++ lib/routes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'home.dart';
+import 'product.dart';
+import 'topScores.dart';
+
+class Routes{
+  static const String home = Home.routeName;
+  static const String product = Product.routeName;
+  static const String topScores = TopScores.routeName;
+    static getRoutes(BuildContext context){
+      return {
+        home:  (context) => Home(),
+       product:  (context) => Product(),
+        topScores:  (context) => TopScores(),};
+    }
+
+
+}
