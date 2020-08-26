cc2023fee18135fcbea0a1f63eb2b2349992369f
Mihail C <calitoiumihail@gmail.com>
2020-08-26 18:25:58 -0400

    new login

---
+++ lib/screens/onboarding/pages/community/community_text_column.dart
@@ -0,0 +1,14 @@
+import 'package:bird/screens/onboarding/widgets/text_column.dart';
+import 'package:flutter/material.dart';
+
+
+class CommunityTextColumn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextColumn(
+      title: 'Community',
+      text:
+          'Eu sint do id aliqua qui tempor sint cillum commodo id voluptate qui.',
+    );
+  }
+}
