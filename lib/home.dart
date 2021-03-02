e15dde6a1b036ca322dd242e8fdf02c194c70a41
Mihail C <calitoiumihail@gmail.com>
2021-03-02 00:45:52 -0500

    small UI changes

--- lib/home.dart
+++ lib/home.dart
@@ -1,3 +1,4 @@
+import 'package:blinking_text/blinking_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/entypo.dart';
@@ -55,102 +56,155 @@ class _HomeState extends State<Home> {
                                             color: Colors.yellowAccent)),
                                   ),
                                   MaterialButton(
-                                    color: Color.fromRGBO(40, 40, 40, 1),
-                                    onPressed: () async {signOut.call();},
-
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                    ),
-                                    child: Icon(Entypo.getIconData("log-out"),color: Colors.yellowAccent,)
-                                  ),
+                                      color: Color.fromRGBO(40, 40, 40, 1),
+                                      onPressed: () async {
+                                        signOut.call();
+                                      },
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                      child: Icon(
+                                        Entypo.getIconData("log-out"),
+                                        color: Colors.yellowAccent,
+                                      )),
                                 ])),
-
                         SizedBox(height: 10),
-                      Container(
-
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(20, 20, 20, 1),
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(20, 20, 20, 1),
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                          ),
+                          child: DataTable(columns: const <DataColumn>[
+                            DataColumn(
+                              label: Text(
+                                'Rank',
+                                style: TextStyle(
+                                    fontStyle: FontStyle.italic,
+                                    fontFamily: 'pixel',
+                                    fontSize: 10,
+                                    color: Colors.white),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Text(
+                                'Username',
+                                style: TextStyle(
+                                    fontStyle: FontStyle.italic,
+                                    fontFamily: 'pixel',
+                                    fontSize: 10,
+                                    color: Colors.white),
+                              ),
+                            ),
+                            DataColumn(
+                              label: Text(
+                                'Score',
+                                style: TextStyle(
+                                    fontStyle: FontStyle.italic,
+                                    fontFamily: 'pixel',
+                                    fontSize: 10,
+                                    color: Colors.white),
+                              ),
+                            ),
+                          ], rows: const <DataRow>[
+                            DataRow(
+                              cells: <DataCell>[
+                                DataCell(Text(
+                                  '1',
+                                  style: TextStyle(
+                                      fontStyle: FontStyle.italic,
+                                      fontFamily: 'pixel',
+                                      fontSize: 12,
+                                      color: Colors.red),
+                                )),
+                                DataCell(Text('snowball',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 10,
+                                        color:
+                                            Color.fromRGBO(200, 200, 200, 1)))),
+                                DataCell(Text('25052',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 10,
+                                        color:
+                                            Color.fromRGBO(200, 200, 200, 1)))),
+                              ],
+                            ),
+                            DataRow(
+                              cells: <DataCell>[
+                                DataCell(Text('2',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 12,
+                                        color: Colors.blue))),
+                                DataCell(Text('aurko',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 10,
+                                        color:
+                                            Color.fromRGBO(200, 200, 200, 1)))),
+                                DataCell(Text('7527',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 10,
+                                        color:
+                                            Color.fromRGBO(200, 200, 200, 1)))),
+                              ],
+                            ),
+                            DataRow(
+                              cells: <DataCell>[
+                                DataCell(Text('3',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 12,
+                                        color: Colors.green))),
+                                DataCell(Text('Becka',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 10,
+                                        color:
+                                            Color.fromRGBO(200, 200, 200, 1)))),
+                                DataCell(Text('05025',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 10,
+                                        color:
+                                            Color.fromRGBO(200, 200, 200, 1)))),
+                              ],
+                            ),
+                            DataRow(
+                              cells: <DataCell>[
+                                DataCell(Text('X',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 12,
+                                        color: Colors.grey))),
+                                DataCell(Text('You',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 10,
+                                        color: Colors.grey))),
+                                DataCell(Text('05025',
+                                    style: TextStyle(
+                                        fontStyle: FontStyle.italic,
+                                        fontFamily: 'pixel',
+                                        fontSize: 10,
+                                        color: Colors.grey))),
+                              ],
+                            ),
+                          ]),
                         ),
-                        child:
-                        DataTable(
-
-                            columns: const <DataColumn>[
-                          DataColumn(
-                            label: Text(
-                              'Rank',
-                              style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                fontSize: 10,color: Colors.white),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              'Username',
-                              style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                  fontSize: 10,color: Colors.white),
-                            ),
-                          ),
-                          DataColumn(
-                            label: Text(
-                              'Score',
-                              style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                  fontSize: 10,color: Colors.white),
-                            ),
-                          ),
-                        ], rows: const <DataRow>[
-                          DataRow(
-                            cells: <DataCell>[
-                              DataCell(Text('1',
-                                style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                    fontSize: 12,color: Colors.red),)),
-                              DataCell(Text('snowball',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 10,color: Color.fromRGBO(200, 200, 200, 1)))),
-                              DataCell(Text('25052',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 10,color: Color.fromRGBO(200, 200, 200, 1)))),
-                            ],
-                          ),
-                          DataRow(
-                            cells: <DataCell>[
-                              DataCell(Text('2',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 12,color: Colors.blue))),
-                              DataCell(Text('aurko',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 10,color: Color.fromRGBO(200, 200, 200, 1)))),
-                              DataCell(Text('7527',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 10,color: Color.fromRGBO(200, 200, 200, 1)))),
-                            ],
-                          ),
-                          DataRow(
-                            cells: <DataCell>[
-                              DataCell(Text('3',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 12,color: Colors.green))),
-                              DataCell(Text('Becka',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 10,color: Color.fromRGBO(200, 200, 200, 1)))),
-                              DataCell(Text('05025',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 10,color: Color.fromRGBO(200, 200, 200, 1)))),
-                            ],
-                          ),
-                          DataRow(
-                            cells: <DataCell>[
-                              DataCell(Text('X',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 12,color: Colors.grey))),
-                              DataCell(Text('You',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 10,color: Colors.grey))),
-                              DataCell(Text('05025',
-                                  style: TextStyle(fontStyle: FontStyle.italic,fontFamily: 'pixel',
-                                      fontSize: 10,color: Colors.grey))),
-                            ],
-                          ),
-                        ]),),
                         SizedBox(
                           height: 10,
                         ),
@@ -174,22 +228,24 @@ class _HomeState extends State<Home> {
                           height: 40,
                         ),
                         RaisedButton(
-                            padding: const EdgeInsets.all(15.0),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            color: Colors.deepPurple,
-                            onPressed: () async {
-                              playGame = true;
-                            },
-                            child: const Text('Insert Token',
-                                style: TextStyle(
-                                  fontSize: 14.0,
-                                  color: Colors.yellowAccent,
-                                  fontFamily: 'pixel',
-                                )),
+                          padding: const EdgeInsets.all(15.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
                           ),
-
+                          color: Colors.deepPurple,
+                          onPressed: () => setState(() {
+                            playGame = true;
+                          }),
+                          child: const BlinkText(
+                            'INSERT TOKEN',
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.yellowAccent,
+                              fontFamily: 'pixel',
+                            ),
+                            endColor: Colors.orangeAccent,
+                          ),
+                        ),
                       ]))));
     } else {
       return MaterialApp(
