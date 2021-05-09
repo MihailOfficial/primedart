ae1e642713c162e4b32c45fffe838b250efe956c
Mihail C <calitoiumihail@gmail.com>
2021-05-08 22:23:52 -0400

    fixed import version problems

--- lib/home.dart
+++ lib/home.dart
@@ -7,8 +7,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_icons/entypo.dart';
-import 'package:flutter_icons/ionicons.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fullscreen/fullscreen.dart';
 import 'package:page_transition/page_transition.dart';
